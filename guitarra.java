3665dd551efd0be3f4c8f0d0e8571b5d78660708
rickdeveloperProjects <ricardohguilherme@gmail.com>
2022-05-04 21:26:25 -0300

    Update guitarra.java
    codigo guitarra.java

--- guitarra.java
+++ guitarra.java
@@ -1,79 +1,72 @@
-public class Guitarra
-{
+package unidade_1:
 
-    private string numeroSerie, fabricante, modelo, tipo, madeira;
-    private double preco;
+//declaração da classe
+public class Guitarra {
 
-    //Criação do contrutor da classe Guitarra
-    public Guitarra(
-            string numeroSerie, string fabricante,
-            string modelo, string tipo,
-            string madeira, double preco)
-    {
+    private String numeroSerie, fabricante, modelo, tipo, madeira;
+    private double preco; 
+
+    //criação do constrututor da classe Guitarra
+    public Guitarra (
+            String numeroSerie, String fabricante,
+            String modelo,      String tipo,
+            String madeira,     double preco) {
         this.numeroSerie = numeroSerie;
         this.fabricante = fabricante;
         this.modelo = modelo;
-        this.tipo = tipo;
+        this.tipo = tipo; 
         this.madeira = madeira;
         this.preco = preco;
+    } 
+    public String getNumeroSerie (){
+        return numeroSerie; 
     }
-    public string getNumeroSerie()
-    {
-        return numeroSerie;
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie; 
     }
-    public void setNumeroSerie(string numeroSerie)
-    {
-        this.numeroSerie = numeroSerie;
+    public String getFabricante() {
+        return fabricante; 
     }
-    public string getFabricante()
-    {
-        return fabricante;
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante; 
     }
-    public void setFabricante(string fabricante)
-    {
-        this.fabricante = fabricante;
+    public String getModelo() {
+        return modelo; 
     }
-    public string getModelo()
-    {
-        return modelo;
+    public void setModelo (String modelo) {
+        this.modelo = modelo;
     }
-    public string getTipo()
-    {
+    public String getTipo() {
         return tipo;
     }
-    public void setTipo(string tipo)
-    {
+    public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    public string getMadeira()
-    {
+    public String getMadeira () {
         return madeira;
     }
-    public void setMadeira(string madeira)
-    {
+    public void setMadeira (String madeira) {
         this.madeira = madeira;
     }
-    public double getPreco()
-    {
+    public double getPreco (){
         return preco;
     }
-    public void setPreco(double preco)
-    {
-        this.preco = preco;
+    public void setPreco(double preco) {
+        this.preco = preco; 
     }
 
-    //O método main ()  é o método principal da classe
-    public static void main(string[] args)
-    {
-        //instanciamoss um objeto chamado "minhaGuitarra" que será do tipo "Guitarra"
-        //Os valores passados são usados pelo construtor da classe para criar o objeto
-        Guitarra minhaGuitarra = new Guitarra("01020304", "Fender", "Telecaster", "Elétrica", "Mogno", 1500);
-        //testando os dados da classe, imprimindo a saída simples no terminal
-        Console.WriteLine("Número de Série: " + (minhaGuitarra.getNumeroSerie()));
-        Console.WriteLine("Madeira: " + (minhaGuitarra.getMadeira()));
-        Console.WriteLine("Preço: " + (minhaGuitarra.getPreco()));
-        Console.WriteLine("Tipo: " + (minhaGuitarra.getTipo()));
-        Console.WriteLine("Fabricante: " + (minhaGuitarra.getFabricante()));
-        Console.WriteLine("Modelo: " + (minhaGuitarra.getModelo()));
+    //o método main() é o método principal da classe 
+    public static void main (String[] args) {
+        //instaciamos um objeto chamado "minhaGuitarra" que será do tipo "Guitarra"
+        //os valores passados são usados pelo construtor da classe para criar o objeto
+        Guitarra minhaGuitarra = new Guitarra ("01020304","fender","telecaster",
+                                                "elétrica","mogno", 1500);
+        //Testando os dados da classe, imprimindo a saída simples no terminal
+        System.out.println(minhaGuitarra.getNumeroSerie());
+        System.out.println(minhaGuitarra.getMadeira());
+        System.out.println(minhaGuitarra.getPreco());
+        System.out.println(minhaGuitarra.getTipo());
+        }                        
+
     }
-}
+ 
